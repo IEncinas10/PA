44341ea467c292e75cfb8e7c4893683f4a9081eb
IEncinas10 <ignacioencinasrubio@gmail.com>
2022-11-22 12:55:39 +0100

    Fix ALU operands width

--- src/alu.sv
+++ src/alu.sv
@@ -4,24 +4,24 @@
   //Revisar esto, en el tuto pone que se puede hacer dentro del decoder pero
   //no se si esto nos sirve a nosotros o no
 module alu #(
-  parameter INSTR_SIZE = `WORD_SIZE 
+  parameter WORD_SIZE = `WORD_SIZE 
 ) (
     input wire [6:0] opcode,
     input wire [6:0] funct7,
     input wire [2:0] funct3,
-    input wire [INSTR_SIZE-1:0] aluIn1, // rs1
-    input wire [INSTR_SIZE-1:0] aluIn2, // rs2 : Iimm ESTO SE CONTROLA FUERA, DENTRO SE TRATA RS2 COMO IMM EN CASO DE SER NECESARIO IMM
-    output reg [INSTR_SIZE-1:0] aluOut,
+    input wire [WORD_SIZE-1:0] aluIn1, // rs1
+    input wire [WORD_SIZE-1:0] aluIn2, // rs2 : Iimm
+    output reg [WORD_SIZE-1:0] aluOut,
     output reg zero, 
-    output reg [INSTR_SIZE-1:0] exceptionCode
+    output reg [WORD_SIZE-1:0] exceptionCode
 );
 
   
 
-  wire [INSTR_SIZE-1:0] aluAdd = aluIn1 + aluIn2;
-  wire [INSTR_SIZE-1:0] aluSub = aluIn1 - aluIn2; 
-  wire [INSTR_SIZE-1:0] aluMul = aluIn1 * aluIn2; //signed????
-  wire [INSTR_SIZE-1:0] noException = 32'b0;
+  wire [WORD_SIZE-1:0] aluAdd = aluIn1 + aluIn2;
+  wire [WORD_SIZE-1:0] aluSub = aluIn1 - aluIn2; 
+  wire [WORD_SIZE-1:0] aluMul = aluIn1 * aluIn2; //signed????
+  wire [WORD_SIZE-1:0] noException = 32'b0;
 
   assign zero = (aluIn1 == aluIn2) ? 1'b1 : 1'b0;
 
