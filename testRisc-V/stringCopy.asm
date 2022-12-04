d7daa72ea5ec2c2eade5f0a3870e5a415c34f54a
Adrian <adrianjg@hotmail.es>
2022-12-04 13:57:06 +0100

    Tests programs for processor: stringCopy & vectorSum

---
+++ testRisc-V/stringCopy.asm
@@ -0,0 +1,25 @@
+.data
+vectorA: .word 1, 2, 3, 4
+vectorB: .word 0, 0, 0, 0
+length: .word 4
+.text
+.global vectorcopy
+vectorcopy:
+	
+	la a0, vectorA # a0 = destination
+	la a1, vectorB # a1 = source
+	lw a2, length # a2 = lenght
+	addi t0, zero, 0
+	addi t1, zero, 0
+	addi t3, zero, 4
+	
+for: 	# For loop
+     	bge   t1, a2, endfor   # if i >= size, break
+    	mul  t2, t1, t3    # Multiply i by 4 (1 << 2 = 4)
+    	add t4, a1, t2     # Update memory addres to copy
+    	add   t2, a0, t2   # Update memory address to read
+    	lw    t2, 0(t2)    # Dereference address to get integer
+	sw t2, 0(t4)	   # Copy value in mem
+    	addi  t1, t1, 1    # Increment the iterator
+    	jal   zero, for     # Jump back to start of loop (1 backwards)
+endfor:
---
+++ testRisc-V/vectorSum.asm
@@ -0,0 +1,27 @@
+# sum elements of a vector in a buffer_sum variable
+
+.data
+vector:
+	.word 1, 2, 3, 4
+length: .word 4
+.section arraysum
+.text
+
+arraysum:
+	la a0, vector
+	lw a1, length
+	addi t0, zero, 0
+	addi t1, zero, 0
+	addi t3, zero, 4
+for: 	# For loop
+     	bge   t1, a1, endfor   # if i >= size, break
+    	mul  t2, t1, t3    # Multiply i by 4 (1 << 2 = 4)
+    	add   t2, a0, t2   # Update memory address
+    	lw    t2, 0(t2)    # Dereference address to get integer
+    	add   t0, t0, t2   # Add integer value to ret
+    	addi  t1, t1, 1    # Increment the iterator
+    	jal   zero, for     # Jump back to start of loop (1 backwards)
+endfor:
+    	addi  a0, t0, 0    # Move t0 (ret) into a0
+    	la a1, length
+	sw a0, 4(a1) ## store in memory the result next to length
