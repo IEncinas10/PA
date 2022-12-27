996a4fa27b9f59df10837f123219c3ad15504af9
IEncinas10 <ignacioencinasrubio@gmail.com>
2022-12-27 21:42:43 +0100

    Memory module added

---
+++ src/memory.sv
@@ -0,0 +1,109 @@
+`include "../defines.sv"
+
+module memory #(
+  parameter MEM_DELAY_CYCLES = `MEM_DELAY_CYCLES,
+  parameter NUM_BLOCKS       = `MEM_SIZE / LINE_SIZE,
+  parameter LINE_SIZE        = `CACHE_LINE_SIZE,
+  parameter WORD_SIZE        = `WORD_SIZE
+) (
+    input wire clk,
+    input wire                 i_read,    // I cache read request and addr
+    input wire [WORD_SIZE-1:0] i_addr,
+    output reg                 i_res,     // I cache response, addr and data
+    output reg [LINE_SIZE-1:0] i_res_data,
+    output reg [WORD_SIZE-1:0] i_res_addr,
+    input wire                 d_read,    // D cache read request and addr
+    input wire [WORD_SIZE-1:0] d_addr,
+    output reg                 d_res,     // D cache response, addr and data
+    output reg [LINE_SIZE-1:0] d_res_data,
+    output reg [WORD_SIZE-1:0] d_res_addr,
+    input wire                 d_wenable,   // D cache write port
+    input wire [LINE_SIZE-1:0] d_w_data,    
+    input wire [WORD_SIZE-1:0] d_w_addr
+);
+
+
+    reg [NUM_BLOCKS] [LINE_SIZE] data;
+
+    reg [MEM_DELAY_CYCLES] [WORD_SIZE-1:0] d_addrs;
+    reg [MEM_DELAY_CYCLES]                 d_reads;
+
+    reg [MEM_DELAY_CYCLES] [WORD_SIZE-1:0] i_addrs;
+    reg [MEM_DELAY_CYCLES]                 i_reads;
+
+    reg [MEM_DELAY_CYCLES]                 d_wenables;
+    reg [MEM_DELAY_CYCLES] [WORD_SIZE-1:0] d_w_addrs;
+    reg [MEM_DELAY_CYCLES] [LINE_SIZE-1:0] d_w_datas;
+
+    
+    integer i;
+
+    initial begin
+        for(i = 0; i < NUM_BLOCKS; i = i + 1) begin
+	    data[i] = 0;
+        end
+
+	for(i = 0; i < MEM_DELAY_CYCLES; i = i + 1) begin
+	    d_addrs[i] = 0;
+	    d_reads[i] = 0;
+
+	    i_addrs[i] = 0;
+	    i_reads[i] = 0;
+
+	    d_wenables[i] = 0;
+	    d_w_addrs[i] = 0;
+	    d_w_datas[i] = 0;
+	end
+    end
+
+    
+
+    reg [WORD_SIZE-1:0] clean_d_addr; 
+    reg [WORD_SIZE-1:0] clean_i_addr; 
+    reg [WORD_SIZE-1:0] clean_w_addr;
+
+    always @(*) begin
+	clean_d_addr = d_addrs >> `OFFSET_SIZE;
+	clean_i_addr = i_addrs >> `OFFSET_SIZE;
+
+	d_res      = d_reads[0];
+	d_res_data = data[clean_d_addr];
+	d_res_addr = d_addrs[0];
+
+	i_res      = i_reads[0];
+	i_res_data = data[clean_i_addr];
+	i_res_addr = i_addrs[0];
+
+    end
+
+
+    always @(posedge(clk)) begin
+	if(d_wenables[0]) begin
+	    clean_w_addr = d_w_addrs[0] >> `OFFSET_SIZE;
+	    data[clean_w_addr] = d_w_datas[0];
+	end
+	for(i = 0; i < MEM_DELAY_CYCLES - 1 ; i = i + 1) begin
+	    d_addrs[i] = d_addrs[i + 1];
+	    d_reads[i] = d_reads[i + 1];
+
+	    i_addrs[i] = i_addrs[i + 1];
+	    i_reads[i] = i_reads[i + 1];
+
+	    d_wenables[i] = d_wenables[i + 1];
+	    d_w_addrs[i]  = d_w_addrs[i + 1];
+	    d_w_datas[i]  = d_w_datas[i + 1];
+	end
+	d_addrs[MEM_DELAY_CYCLES - 1] = d_addr;
+	d_reads[MEM_DELAY_CYCLES - 1] = d_read;
+
+	i_addrs[MEM_DELAY_CYCLES - 1] = i_addr;
+	i_reads[MEM_DELAY_CYCLES - 1] = i_read;
+
+	d_wenables[MEM_DELAY_CYCLES - 1] = d_wenable;
+	d_w_addrs[MEM_DELAY_CYCLES - 1]  = d_w_addr;
+	d_w_datas[MEM_DELAY_CYCLES - 1]  = d_w_data;
+
+    end
+
+   
+endmodule
---
+++ src/tests/memory_testbench.sv
@@ -0,0 +1,121 @@
+// Mandatory file to be able to launch SVUT flow
+`include "svut_h.sv"
+// Specify the module to load or on files.f
+`include "memory.sv"
+`timescale 1 ns / 1 ns
+
+module memory_testbench();
+
+    `SVUT_SETUP
+
+    parameter MEM_DELAY_CYCLES= `MEM_DELAY_CYCLES;
+    parameter NUM_BLOCKS      = `MEM_SIZE / LINE_SIZE;
+    parameter LINE_SIZE       = `CACHE_LINE_SIZE;
+    parameter WORD_SIZE       = `WORD_SIZE;
+
+    logic clk;
+    logic                 i_read;
+    logic [WORD_SIZE-1:0] i_addr;
+    logic                i_res;
+    logic[LINE_SIZE-1:0] i_res_data;
+    logic[WORD_SIZE-1:0] i_res_addr;
+    logic                 d_read;
+    logic [WORD_SIZE-1:0] d_addr;
+    logic                d_res;
+    logic[LINE_SIZE-1:0] d_res_data;
+    logic[WORD_SIZE-1:0] d_res_addr;
+    logic                 d_wenable;
+    logic [LINE_SIZE-1:0] d_w_data;
+    logic [WORD_SIZE-1:0] d_w_addr;
+
+    memory 
+    #(
+    .MEM_DELAY_CYCLES       (MEM_DELAY_CYCLES),
+    .NUM_BLOCKS      (NUM_BLOCKS),
+    .LINE_SIZE       (LINE_SIZE),
+    .WORD_SIZE       (WORD_SIZE)
+    )
+    dut 
+    (
+    .clk        (clk),
+    .i_read     (i_read),
+    .i_addr     (i_addr),
+    .i_res      (i_res),
+    .i_res_data (i_res_data),
+    .i_res_addr (i_res_addr),
+    .d_read     (d_read),
+    .d_addr     (d_addr),
+    .d_res      (d_res),
+    .d_res_data (d_res_data),
+    .d_res_addr (d_res_addr),
+    .d_wenable  (d_wenable),
+    .d_w_data   (d_w_data),
+    .d_w_addr   (d_w_addr)
+    );
+
+
+    // To create a clock:
+     initial clk = 0;
+     always #1 clk = ~clk;
+
+    // To dump data for visualization:
+     initial begin
+	 $dumpfile("memory_testbench.vcd");
+	 $dumpvars(0, memory_testbench);
+     end
+
+    // Setup time format when printing with $realtime()
+    initial $timeformat(-9, 1, "ns", 8);
+
+    task setup(msg="");
+    begin
+        // setup() runs when a test begins
+    end
+    endtask
+
+    task teardown(msg="");
+    begin
+        // teardown() runs when a test ends
+    end
+    endtask
+
+    `TEST_SUITE("TESTSUITE_NAME")
+
+    //  Available macros:"
+    //
+    //    - `MSG("message"):       Print a raw white message
+    //    - `INFO("message"):      Print a blue message with INFO: prefix
+    //    - `SUCCESS("message"):   Print a green message if SUCCESS: prefix
+    //    - `WARNING("message"):   Print an orange message with WARNING: prefix and increment warning counter
+    //    - `CRITICAL("message"):  Print a purple message with CRITICAL: prefix and increment critical counter
+    //    - `ERROR("message"):     Print a red message with ERROR: prefix and increment error counter
+    //
+    //    - `FAIL_IF(aSignal):                 Increment error counter if evaluaton is true
+    //    - `FAIL_IF_NOT(aSignal):             Increment error coutner if evaluation is false
+    //    - `FAIL_IF_EQUAL(aSignal, 23):       Increment error counter if evaluation is equal
+    //    - `FAIL_IF_NOT_EQUAL(aSignal, 45):   Increment error counter if evaluation is not equal
+    //    - `ASSERT(aSignal):                  Increment error counter if evaluation is not true
+    //    - `ASSERT((aSignal == 0)):           Increment error counter if evaluation is not true
+    //
+    //  Available flag:
+    //
+    //    - `LAST_STATUS: tied to 1 is last macro did experience a failure, else tied to 0
+
+    `UNIT_TEST("TESTCASE_NAME")
+
+	dut.data[1] = 128'h12345678123456781234567812345678;
+	i_read = 1;	
+	i_addr = (1 << 4);
+
+	#(2 * `MEM_DELAY_CYCLES);
+
+
+	#2;
+
+
+
+    `UNIT_TEST_END
+
+    `TEST_SUITE_END
+
+endmodule
