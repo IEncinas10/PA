9adb385dd898ec107c34b6d4def27d486dbe3a57
Adrian <adrianjg@hotmail.es>
2023-01-22 14:05:38 +0100

    files.f

---
+++ src/tests/testbenchs/matrix/files.f
@@ -0,0 +1,8 @@
++incdir+/home/ier/miri/pa/practica/src/
++incdir+/home/ier/miri/pa/practica/src/tests/
++incdir+/home/ier/miri/pa/practica/src/tests/testbenchs/
++incdir+/home/ier/miri/pa/practica/src/tests/testbenchs/matrix/
++incdir+/home/adrian/upc/pa/PA/src/
++incdir+/home/adrian/upc/pa/PA/src/tests/
++incdir+/home/adrian/upc/pa/PA/src/tests/testbenchs/
++incdir+/home/adrian/upc/pa/PA/src/tests/testbenchs/matrix/
