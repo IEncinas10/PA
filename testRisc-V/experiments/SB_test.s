0d280cb31c987f05a445fe7d24ffcba3ff6d0bb9
IEncinas10 <ignacioencinasrubio@gmail.com>
2023-01-23 16:51:50 +0100

    Add comments clarifying lines for Store buffer bypass test

--- testRisc-V/experiments/SB_test.s
+++ testRisc-V/experiments/SB_test.s
@@ -5,25 +5,30 @@ position:
 .global main
 
 main:
+#Line 0
 	la a0, position
 	addi a5, a5, 0xff
 	nop
+#Line 1
 	#sw-lw
 	sw a5, 0(a0)
 	lw a4, 0(a0)
 	nop
 	nop
+#Line 2
 	#sw-lb
 	addi a5, a5, 2
 	sw a5, 128(a0)
 	lb a4, 128(a0)
 	lb a3, 129(a0)
+#Line 3
 	addi a4, a4, 1
 	#sb-lb
 	sb a4, 256(a0)
 	lb a4, 256(a0)
 	#sb-lw
 	addi a4, a4, 1
+#Line 4
 	nop
 	sb a4, 512(a0)
 	lw a5, 512(a0)
