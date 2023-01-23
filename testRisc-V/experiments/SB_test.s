80d2d1887a703210f3b3c05040d8dd53c0af13e2
Adrian <adrianjg@hotmail.es>
2023-01-23 16:49:48 +0100

    Test SB modification

--- testRisc-V/experiments/SB_test.s
+++ testRisc-V/experiments/SB_test.s
@@ -6,11 +6,13 @@ position:
 
 main:
 	la a0, position
-	
 	addi a5, a5, 0xff
+	nop
 	#sw-lw
 	sw a5, 0(a0)
 	lw a4, 0(a0)
+	nop
+	nop
 	#sw-lb
 	addi a5, a5, 2
 	sw a5, 128(a0)
@@ -22,6 +24,7 @@ main:
 	lb a4, 256(a0)
 	#sb-lw
 	addi a4, a4, 1
+	nop
 	sb a4, 512(a0)
 	lw a5, 512(a0)
 end: 
